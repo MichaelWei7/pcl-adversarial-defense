4877526d456b0bcb6517e2b3f14bad65c4959b5a
Michael W <weipf8@163.com>
2020-10-28 19:20:37 +0800

    Update contrastive_proximity.py

--- contrastive_proximity.py
+++ contrastive_proximity.py
@@ -15,30 +15,31 @@ class Con_Proximity(nn.Module):
             self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))
 
     def forward(self, x, labels):
-
         batch_size = x.size(0)
-        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
-                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
-        distmat.addmm_(1, -2, x, self.centers.t())
 
+        '''
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+        torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+        distmat.addmm_(1, -2, x, self.centers.t())
+        '''
+
+
+        distmat = torch.norm(x[:, None, :] - self.centers[None, :, :], dim = 2) ** 2
         classes = torch.arange(self.num_classes).long()
+
+
         if self.use_gpu: classes = classes.cuda()
         labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
         mask = labels.eq(classes.expand(batch_size, self.num_classes))
 
         dist = []
         for i in range(batch_size):
+            k = mask[i].clone().to(dtype=torch.long)
+            k = 1-k
+            kk = k.clone().to(dtype=torch.long)
 
-            k= mask[i].clone().to(dtype=torch.int8)
-
-            k= -1* k +1
-
-            kk= k.clone().to(dtype=torch.uint8)
-
-            value = distmat[i][kk]
-
+            value = distmat[i][kk] # 找出非对应类别的距离
             value = value.clamp(min=1e-12, max=1e+12) # for numerical stability
-
             dist.append(value)
         dist = torch.cat(dist)
         loss = dist.mean()
