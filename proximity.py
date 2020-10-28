e5f73f6a407b4b6956590db7a8e57526a34de330
Michael W <weipf8@163.com>
2020-10-28 19:20:49 +0800

    Update proximity.py

--- proximity.py
+++ proximity.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 
 class Proximity(nn.Module):
-
+    '''
+    计算对应类别的距离损失
+    '''
     def __init__(self, num_classes=100, feat_dim=1024, use_gpu=True):
         super(Proximity, self).__init__()
         self.num_classes = num_classes
@@ -15,11 +17,19 @@ class Proximity(nn.Module):
             self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim))
 
     def forward(self, x, labels):
-
         batch_size = x.size(0)
-        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + 
-                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+
+        '''
+        # 计算距离, 方法一
+        distmat = torch.pow(x,            2).sum(dim=1, keepdim = True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim = True).expand(self.num_classes, batch_size).t()
         distmat.addmm_(1, -2, x, self.centers.t())
+        '''
+        # 计算距离, 方法二
+
+        distmat = torch.norm(x[:, None, :] - self.centers[None, :, :], dim = 2) ** 2
+
+
 
         classes = torch.arange(self.num_classes).long()
         if self.use_gpu: classes = classes.cuda()
@@ -28,8 +38,8 @@ class Proximity(nn.Module):
 
         dist = []
         for i in range(batch_size):
-            value = distmat[i][mask[i]]
-            value = value.clamp(min=1e-12, max=1e+12) 
+            value = distmat[i][mask[i]] # 获取对应类别的距离
+            value = value.clamp(min=1e-12, max=1e+12)
             dist.append(value)
         dist = torch.cat(dist)
         loss = dist.mean()
